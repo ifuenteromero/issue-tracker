e150cf4636caefd947369c9434b9e945482c322a
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-09 17:01:12 +0200

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const IssuesPage = () => {
 	return (
 		<div>
-			<p>IssuesPage</p>
-			<Button>New Issue</Button>
+			<Button>
+				<Link href='/issues/new'>New Issue</Link>
+			</Button>
 		</div>
 	);
 };
