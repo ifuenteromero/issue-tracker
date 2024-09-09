79b05079e9eda8e4247f6a26eb3ee19c356ca41b
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-09 16:45:40 +0200

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@radix-ui/themes';
+
 const IssuesPage = () => {
-	return <div>IssuesPage</div>;
+	return (
+		<div>
+			<p>IssuesPage</p>
+			<Button>New Issue</Button>
+		</div>
+	);
 };
 
 export default IssuesPage;
