0b427b10c65861b110f0f754ee2690e9e3ad0342
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-10 20:12:39 +0200

    Show issue detail

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,23 @@
-const IssueDetailPage = () => {
-	return <div>IssueDetailPage</div>;
+import prisma from '@/prisma/client';
+import { notFound } from 'next/navigation';
+
+const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
+	const issueId = parseInt(params.id);
+	if (typeof issueId !== 'number' || isNaN(issueId)) notFound();
+
+	const issue = await prisma.issue.findUnique({
+		where: { id: issueId },
+	});
+
+	if (!issue) notFound();
+	return (
+		<div>
+			<p>{issue.title}</p>
+			<p>{issue.description}</p>
+			<p>{issue.status}</p>
+			<p>{issue.createdAt.toDateString()}</p>
+		</div>
+	);
 };
 
 export default IssueDetailPage;
