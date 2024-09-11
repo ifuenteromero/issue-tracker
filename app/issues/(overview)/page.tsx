01e04a0cecfa0095479a573ca56b7f62a897cc70
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-11 19:43:45 +0200

    Refactor: Organize imports on index file

--- app/issues/(overview)/page.tsx
+++ app/issues/(overview)/page.tsx
@@ -1,8 +1,7 @@
+import { IssueStatusBadge, Link } from '@/app/components';
 import routes from '@/app/utils/routes';
 import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
-import IssueStatusBadge from '../../components/IssueStatusBadge';
-import Link from '../../components/Link';
 import IssueActions from '../IssueActions';
 
 const IssuesPage = async () => {
