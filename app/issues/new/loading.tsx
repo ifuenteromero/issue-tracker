79ffc420c6263a04ba8cc81cb27260f426aa1295
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-11 14:16:24 +0200

    Add Loading Skeleton to New Issue Page

---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,19 @@
+import { Button, Skeleton, TextArea, TextField } from '@radix-ui/themes';
+
+const LoadingNewIssuePage = async () => {
+	return (
+		<div className='max-w-xl flex flex-col gap-2'>
+			<Skeleton>
+				<TextField.Root />
+			</Skeleton>
+			<Skeleton>
+				<TextArea rows={10} />
+			</Skeleton>
+			<Skeleton width='9rem'>
+				<Button>Submit New Issue</Button>
+			</Skeleton>
+		</div>
+	);
+};
+
+export default LoadingNewIssuePage;
