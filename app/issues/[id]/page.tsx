6f8916d6515b348723ceadc6b359b17d70412592
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-12 18:23:53 +0200

    Add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { IssueStatusBadge } from '@/app/components';
+import routes from '@/app/utils/routes';
 import prisma from '@/prisma/client';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import { HiOutlinePencilSquare } from 'react-icons/hi2';
 import Markdown from 'react-markdown';
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
@@ -14,16 +17,29 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
 
 	if (!issue) notFound();
 	return (
-		<Flex direction='column' gap='2' className='max-w-xl'>
-			<Heading>{issue.title}</Heading>
-			<Flex gap='2' align='center' wrap='wrap'>
-				<IssueStatusBadge status={issue.status} />
-				<Text>{issue.createdAt.toDateString()}</Text>
+		<Grid columns={{ initial: '1', md: '2' }} gapY='5'>
+			<Flex direction='column' gap='2' className='max-w-xl'>
+				<Heading>{issue.title}</Heading>
+				<Flex gap='2' align='center' wrap='wrap'>
+					<IssueStatusBadge status={issue.status} />
+					<Text>{issue.createdAt.toDateString()}</Text>
+				</Flex>
+				<Card className='prose'>
+					<Markdown>{issue.description}</Markdown>
+				</Card>
 			</Flex>
-			<Card className='prose'>
-				<Markdown>{issue.description}</Markdown>
-			</Card>
-		</Flex>
+			<Box>
+				<Button className='!cursor-pointer !p-0'>
+					<Link
+						href={routes.editIssue(params.id)}
+						className='flex items-center gap-2 h-full px-3'
+					>
+						<HiOutlinePencilSquare />
+						Edit Issue
+					</Link>
+				</Button>
+			</Box>
+		</Grid>
 	);
 };
 
--- app/utils/routes.ts
+++ app/utils/routes.ts
@@ -2,12 +2,14 @@ const root = '/';
 const issues = '/issues';
 const newIssue = `${issues}/new`;
 const issueDetail = (id: string) => `${issues}/${id}`;
+const editIssue = (id: string) => `${issues}/${id}/edit`;
 
 const routes = {
 	root,
 	issues,
 	newIssue,
 	issueDetail,
+	editIssue,
 };
 
 export default routes;
