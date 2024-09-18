6d3e4f1eafee5b1474d90a31bf8b361e8bc1e881
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-18 20:04:59 +0200

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,13 +1,20 @@
+import { prisma } from '@/prisma/client';
 import { Select } from '@radix-ui/themes';
 
-const AssigneeSelect = () => {
+const AssigneeSelect = async () => {
+	const users = await prisma.user.findMany();
+
 	return (
 		<Select.Root>
 			<Select.Trigger placeholder='Assign...' />
 			<Select.Content>
 				<Select.Group>
 					<Select.Label>Suggestions</Select.Label>
-					<Select.Item value='1'>Irene</Select.Item>
+					{users.map((user) => (
+						<Select.Item key={user.id} value={user.id}>
+							{user.name}
+						</Select.Item>
+					))}
 				</Select.Group>
 			</Select.Content>
 		</Select.Root>
