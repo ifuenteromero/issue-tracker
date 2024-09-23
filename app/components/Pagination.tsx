4ba4535d78a3c9f92fbd142669f1f20b9a595647
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-23 19:50:32 +0200

    Implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
 import {
 	MdOutlineFirstPage,
 	MdOutlineLastPage,
@@ -14,29 +17,50 @@ interface Props {
 
 const Pagination = ({ itemsCount, currentPage, pageSize }: Props) => {
 	const pageCount = Math.ceil(itemsCount / pageSize);
+	const searchParams = useSearchParams();
+	const router = useRouter();
+
+	const changePage = (page: number) => {
+		const params = new URLSearchParams(searchParams);
+		params.set('page', page.toString());
+		router.push('?' + params.toString());
+	};
+	if (currentPage > pageCount || currentPage < 1) changePage(1);
 	if (pageCount <= 1) return null;
 	return (
 		<Flex align='center' gap='3'>
 			<Text>
 				Page {currentPage} of {pageCount}{' '}
 			</Text>
-			<Button color='gray' variant='soft' disabled={currentPage === 1}>
+			<Button
+				onClick={() => changePage(1)}
+				color='gray'
+				variant='soft'
+				disabled={currentPage === 1}
+			>
 				<MdOutlineFirstPage />
 			</Button>
-			<Button color='gray' variant='soft' disabled={currentPage === 1}>
+			<Button
+				onClick={() => changePage(currentPage - 1)}
+				color='gray'
+				variant='soft'
+				disabled={currentPage === 1}
+			>
 				<MdOutlineNavigateBefore />
 			</Button>
 			<Button
 				color='gray'
 				variant='soft'
-				disabled={currentPage === pageCount}
+				disabled={currentPage >= pageCount}
+				onClick={() => changePage(currentPage + 1)}
 			>
 				<MdOutlineNavigateNext />
 			</Button>
 			<Button
 				color='gray'
 				variant='soft'
-				disabled={currentPage === pageCount}
+				disabled={currentPage >= pageCount}
+				onClick={() => changePage(pageCount)}
 			>
 				<MdOutlineLastPage />
 			</Button>
