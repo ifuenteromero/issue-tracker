4acf49f1cb7ca5128f8a59200cf33f06a796875a
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-24 19:47:38 +0200

    Add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,7 +7,11 @@ import DeleteIssueButton from './DeleteIssueButton';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
 
-const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
+interface Props {
+	params: { id: string };
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
 	const issueId = parseInt(params.id);
 	if (typeof issueId !== 'number' || isNaN(issueId)) notFound();
 
@@ -33,6 +37,16 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
 			)}
 		</Grid>
 	);
+};
+
+export const generateMetadata = async ({ params }: Props) => {
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+	return {
+		title: issue?.title,
+		description: 'Details of issue ' + issue?.id,
+	};
 };
 
 export default IssueDetailPage;
