da34310037103e0039e541ba0d0ff73950fa946b
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-09-24 16:36:25 +0200

    Build the IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { prisma } from '@/prisma/client';
+import { Flex } from '@radix-ui/themes';
+import IssueSummary from './issues/_components/IssueSummary';
 import LatestIssues from './issues/_components/LatestIssues';
 
-const Home = () => {
-	return <LatestIssues />;
+const Home = async () => {
+	const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+	const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
+	const inProgress = await prisma.issue.count({
+		where: { status: 'IN_PROGRESS' },
+	});
+
+	return (
+		<Flex direction='column' gap='4'>
+			<IssueSummary open={open} closed={closed} inProgress={inProgress} />
+			<LatestIssues />
+		</Flex>
+	);
 };
 
 export default Home;
