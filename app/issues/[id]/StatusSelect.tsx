47b10e0c22376050cf1d387b1ba52a8a255f84a8
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-10-02 18:15:25 +0200

    Refresh page after successful status update to ensure status badge is updated

--- app/issues/[id]/StatusSelect.tsx
+++ app/issues/[id]/StatusSelect.tsx
@@ -5,6 +5,7 @@ import { statusMap } from '@/app/components/IssueStatusBadge';
 import { Issue, Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import toast, { Toaster } from 'react-hot-toast';
 
 const StatusSelect = ({ issue }: { issue: Issue }) => {
@@ -12,12 +13,18 @@ const StatusSelect = ({ issue }: { issue: Issue }) => {
 		statusMap
 	).map(([status, { label }]) => ({ label, status: status as Status }));
 
+	const router = useRouter();
+
 	const changeStatus = async (value: Status) => {
 		const endpoint = endpoints.issueDetail(issue.id.toString());
 		toast.promise(
-			axios.patch(endpoint, {
-				status: value,
-			}),
+			axios
+				.patch(endpoint, {
+					status: value,
+				})
+				.then(() => {
+					router.refresh();
+				}),
 			{
 				loading: 'Updating status...',
 				success: 'Status updated!',
@@ -35,7 +42,6 @@ const StatusSelect = ({ issue }: { issue: Issue }) => {
 				<Select.Trigger placeholder='Status...' />
 				<Select.Content>
 					<Select.Group>
-						<Select.Label>Suggestions</Select.Label>
 						{statuses.map(({ label, status }) => (
 							<Select.Item key={status} value={status}>
 								{label}
